fb8c79756916aebfed0c0f499edef516ac544e76
canknbr <cankanbur1@gmail.com>
2023-04-03 03:55:46 +0300

    Initial Commit

---
+++ RecordPlayer/RecordPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordPlayerApp.swift
+//  RecordPlayer
+//
+//  Created by Can Kanbur on 3.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct RecordPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
