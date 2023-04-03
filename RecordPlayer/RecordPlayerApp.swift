f58e71b0cb6d6cd37b50a135eff0d4d2629dd497
canknbr <cankanbur1@gmail.com>
2023-04-03 03:57:30 +0300

    init

--- RecordPlayer/RecordPlayerApp.swift
+++ RecordPlayer/RecordPlayerApp.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 @main
 struct RecordPlayerApp: App {
+    @State var rotateRecord : Bool = false
+    @State var rotateArm : Bool = false
+    @State var shouldAnimate : Bool = false
+    @State var degrees : Double = 0.0
     var body: some Scene {
         WindowGroup {
             ContentView()
